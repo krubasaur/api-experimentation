88401fb8b3bca3d16bdc801fac59fb10b82c0f0a
krubasaur <alana.krubl@gmail.com>
2018-03-27 16:17:09 -0700

    refactoring so api.get() parameters can be fed to requests.get() param feild per get request.

--- tumblr/tests.py
+++ tumblr/tests.py
@@ -25,19 +25,31 @@ def print_tagged_posts(tag, limit):
             \t* Date Posted: {date}."""
             )
         post_count =+ 1
-        
-def main():
-    nl = os.linesep
-    linebreak = nl * 2 + '=' * 80 + nl * 2
 
-    print(linebreak + 'TAGGED POSTS TEST:' + nl)
+
+def print_blog_posts():
     request = api.Client()
-    tagged_posts = request.get_tagged_posts('smile', 2)
 
-    print(tagged_posts) # prints entire response contnets
+    blog_name = str(input('Blog name:  '))
+    posts = request.get_blog_posts(blog_name)#, params={'limit': '5'})
 
-    print_tagged_posts('happy', 2)
+    print(posts['response']['posts'][0]['id'])
+    print(posts['response']['posts'][1]['id'])
+    print(posts['response']['posts'][2]['id'])
+    print(posts['response']['posts'][3]['id'])
 
+def main():
+    # nl = os.linesep
+    # linebreak = nl * 2 + '=' * 80 + nl * 2
+    #
+    # print(linebreak + 'TAGGED POSTS TEST:' + nl)
+    # request = api.Client()
+    # tagged_posts = request.get_tagged_posts('smile', 2)
+    #
+    # # print(tagged_posts) # prints entire response contnets
+
+    # print_tagged_posts('happy', 2)
+    print_blog_posts()
 
 if __name__ == '__main__':
     main()
