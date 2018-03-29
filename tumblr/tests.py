e3881fdbcf836f2698bb5304331a0516e9d940fb
krubasaur <alana.krubl@gmail.com>
2018-03-28 19:37:27 -0700

    attempting to have tests.py print getting tagged posts or a specific blogs posts.

--- tumblr/tests.py
+++ tumblr/tests.py
@@ -1,11 +1,11 @@
 import os
 
-import main, api
+import credentials, main, api
 
 
 def print_tagged_posts(tag, limit):
     request = api.Client()
-    posts = request.get_tagged_posts(tag, limit)
+    posts = request.get_tagged_posts(tag, params={'limit': limit})
 
     post_count = 0
 
@@ -31,24 +31,23 @@ def print_blog_posts():
     request = api.Client()
 
     blog_name = str(input('Blog name:  '))
-    posts = request.get_blog_posts(blog_name)#, params={'limit': '5'})
+    limit = str(input('# of results:  '))
+    posts = request.get_blog_posts(blog_name, params={'api_key': credentials.consumer_key, 'limit': limit})
+    post_count = 0
+    for post in posts:
+        print(f'Post #{post_count}:')
+        print(posts['response']['posts'][post_count]['id'])
+        post_count =+ 1
 
-    print(posts['response']['posts'][0]['id'])
-    print(posts['response']['posts'][1]['id'])
-    print(posts['response']['posts'][2]['id'])
-    print(posts['response']['posts'][3]['id'])
 
 def main():
-    # nl = os.linesep
-    # linebreak = nl * 2 + '=' * 80 + nl * 2
-    #
-    # print(linebreak + 'TAGGED POSTS TEST:' + nl)
-    # request = api.Client()
-    # tagged_posts = request.get_tagged_posts('smile', 2)
-    #
-    # # print(tagged_posts) # prints entire response contnets
+    nl = os.linesep
+    linebreak = nl * 2 + '=' * 80 + nl * 2
 
-    # print_tagged_posts('happy', 2)
+    print(linebreak + 'TAGGED POSTS TEST:' + nl)
+
+
+    print_tagged_posts('happy', '2')
     print_blog_posts()
 
 if __name__ == '__main__':
