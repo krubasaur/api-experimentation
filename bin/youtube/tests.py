adcb7075022d015214283a8ae0273cec632956e8
krubasaur <alana.krubl@gmail.com>
2018-04-03 17:01:59 -0700

    updated print_channel_info() to test using different params than just an id

--- bin/youtube/tests.py
+++ bin/youtube/tests.py
@@ -12,15 +12,19 @@ class Tests(object):
     def __init__(self):
         print('class initiated')
 
-    def print_channel_info(self, client, part, id):
-        params = dict(part=part, id=id)
+    def print_channel_info(self, client, part, filter):
+        params = dict(part=part, **filter)
         response = client.get_channel_info(params)
         print(response['items'])
 
 def main():
     youtube = Client(os.environ['youtube_api_key'])
     test1 = Tests()
-    test1.print_channel_info(youtube, 'snippet', 'UC_x5XG1OV2P6uZZ5FSM9Ttw')
+    test1.print_channel_info(youtube, 'snippet', {'id': 'UC_x5XG1OV2P6uZZ5FSM9Ttw'})
+
+    test2 = Tests()
+    test2.print_channel_info(youtube, 'contentDetails', {'forUsername': 'schmoyoho' })
+    test2.print_channel_info(youtube, 'snippet', {'id': 'UCNYrK4tc5i1-eL8TXesH2pg'})
 
 if __name__ == '__main__':
     main()
