9b3f096903e01b119d9c7fefd33b75c1e7b13fb5
krubasaur <alana.krubl@gmail.com>
2018-06-19 20:50:05 -0700

    consolidating tests into test class. need to come back and fix to make this not fail anymore.

--- lib/imgur/api.py
+++ lib/imgur/api.py
@@ -1,28 +1,34 @@
 import requests
 
-def get_subreddit(url, clientId):
-    try:
-        response = requests.get(
-            url=f'{url}',
-            headers={
-                'Authorization': f'Client-ID {clientId}',
-            }
-        )
-        print(f'Status: {response.status_code}')
-        print(f'Body: {response.content}')
-    except requests.exceptions.RequestException:
-        print('Failed')
+class Client(object):
+    host = 'https://api.imgur.com/3/gallery/'
 
-# need to see what data we're looking for. will consolidate these GET reqs soon.
-def get_post_data(url, clientId):
-    try:
-        response = requests.get(
-            url=f'{url}',
-            headers={
-                'Authorization': f'Client-ID {clientId}',
-            }
-        )
-        print(f'Status: {response.status_code}')
-        print(str(f'Data: {response.content[1]}'))
-    except requests.exceptions.RequestException:
-        print('Failed')
+    def __init__(self, clientId):
+        self.clientId = clientId
+
+    def get_subreddit(url, clientId):
+        try:
+            response = requests.get(
+                url=f'r/{url}',
+                headers={
+                    'Authorization': f'Client-ID {clientId}',
+                }
+            )
+            print(f'Status: {response.status_code}')
+            print(f'Body: {response.content}')
+        except requests.exceptions.RequestException:
+            print('Failed')
+
+    # need to see what data we're looking for. will consolidate these GET reqs soon.
+    def get_post_data(url, clientId):
+        try:
+            response = requests.get(
+                url=f'r/{url}',
+                headers={
+                    'Authorization': f'Client-ID {clientId}',
+                }
+            )
+            print(f'Status: {response.status_code}')
+            print(str(f'Data: {response.content[1]}'))
+        except requests.exceptions.RequestException:
+            print('Failed')
